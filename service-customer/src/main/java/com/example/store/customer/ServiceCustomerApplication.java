2c179f0e1b9ce82829dc8cad6489e596fb2ff568
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 16:08:06 -0500

    quitar espacios

--- service-customer/src/main/java/com/example/store/customer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/com/example/store/customer/ServiceCustomerApplication.java
@@ -2,8 +2,10 @@ package com.example.store.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceCustomerApplication {
 
 	public static void main(String[] args) {
--- service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
+++ service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 //import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 //@EnableFeignClients
 //@EnableHystrix
 //@EnableHystrixDashboard
+@EnableEurekaClient
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {

f43e68faf0b4b254425f2014e21fe6349e45efbd
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 15:37:54 -0500

    se agrega el proyecto con la configuracion de Eureka service se agrega el archivo de config-data/registry-service.yml

---
+++ registry-service/src/test/java/com/example/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

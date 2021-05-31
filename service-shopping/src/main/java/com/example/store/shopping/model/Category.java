a0162f99027c386112ca0eb9b6e8143692a21e90
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 18:35:05 -0500

    test entity

--- service-shopping/src/main/java/com/example/store/shopping/model/Category.java
+++ service-shopping/src/main/java/com/example/store/shopping/model/Category.java
@@ -1,10 +1,21 @@
 package com.example.store.shopping.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
+@Entity
+@Table(name= "tbl_categories")
 @Data
 public class Category {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-
+	
 }
--- service-shopping/src/main/java/com/example/store/shopping/model/Region.java
+++ service-shopping/src/main/java/com/example/store/shopping/model/Region.java
@@ -1,9 +1,21 @@
 package com.example.store.shopping.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "tbl_regions")
 public class Region {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 }

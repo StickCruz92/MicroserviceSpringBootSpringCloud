da4e36cf0da86d178ecde5097a2278313cd39808
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 22:14:08 -0500

    agragar configuracion hystrix

--- service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
+++ service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {

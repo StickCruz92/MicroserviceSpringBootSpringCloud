d6ed3fda82196f05b45b7dbe17845c65e7271015
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 22:49:30 -0500

    hystrix

--- service-shopping/src/main/java/com/example/store/shopping/client/CustomerClient.java
+++ service-shopping/src/main/java/com/example/store/shopping/client/CustomerClient.java
@@ -1,16 +1,13 @@
 package com.example.store.shopping.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.store.shopping.model.Customer;
 
-@FeignClient(name = "service-customer")
-@RequestMapping("/customers")
+@FeignClient(name = "service-customer", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
 
     @GetMapping(value = "/{id}")
--- service-shopping/src/main/java/com/example/store/shopping/model/Customer.java
+++ service-shopping/src/main/java/com/example/store/shopping/model/Customer.java
@@ -1,9 +1,10 @@
 package com.example.store.shopping.model;
 
 
+import lombok.Builder;
 import lombok.Data;
 
-@Data
+@Data @Builder
 public class Customer {
 
     private Long id;

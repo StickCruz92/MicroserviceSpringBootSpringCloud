160fbb9cf7ca36895443bc654c0be6e7e9a3f04c
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 23:45:53 -0500

    actuator y admin

---
+++ admin-service/src/test/java/com/example/store/admin/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store.admin.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

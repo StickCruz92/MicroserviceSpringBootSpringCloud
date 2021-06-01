7712cb246fdf13e133c45548ee1de870ac2f7062
Stick cruz <stick.cruz1992@gmail.com>
2021-05-31 20:22:13 -0500

    test 4

--- service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
+++ service-shopping/src/main/java/com/example/store/shopping/ServiceShoppingApplication.java
@@ -3,11 +3,11 @@ package com.example.store.shopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
-@EnableEurekaClient
 public class ServiceShoppingApplication {
 
 	public static void main(String[] args) {
